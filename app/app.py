c08fdb70b3edd74d3c04692859e6f613c1e479ba
Jack Edwards <jackedwards@protonmail.com>
2021-03-27 17:08:17 -0500

    Show space available on webpage

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ from pathlib import Path
 from modules import settings
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 # Load settings
 this_file = Path(__file__)
@@ -23,7 +24,29 @@ app.config['database'] = db
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+    gb_allocated = float(app.config['crypter_config']['APP']['StorageLimit'])
+    
+    bytes_used = 0
+    start_path = app.config['crypter_config']['PATH']['Files']
+    for path, dirs, files in os.walk(start_path):
+        for f in files:
+            fp = os.path.join(path, f)
+            bytes_used += os.path.getsize(fp)
+
+    bytes_allocated = gb_allocated * 1024 * 1024 * 1024
+    percent_remaining = 100 - (100 * (bytes_used / bytes_allocated))
+    bytes_remaining = bytes_allocated - bytes_used
+
+    labels = ['Bytes', 'KB', 'MB', 'GB', 'TB']
+    label_index = 0
+    while (bytes_remaining > 1024):
+        bytes_remaining /= 1024
+        label_index += 1
+
+    return render_template('index.html',
+                            percentDiskRemaining=round(percent_remaining, 1),
+                            bytesDiskRemaining=f'{round(bytes_remaining, 2)} {labels[label_index]}',
+                            bytesDiskAllocated=f'{round(gb_allocated, 2)} GB')
 
 from routes import file_routes
 app.register_blueprint(file_routes, url_prefix='/file')
